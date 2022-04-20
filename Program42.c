06accf427e84c333e238e7f51dcb1a80c9fc71ec
Shashankrishna <47184553+Shashankrishna@users.noreply.github.com>
2022-04-20 04:02:53 -0400

    Add files via upload

---
+++ Program42.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  char greetings[] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!', '\0'};
+  char greetings2[] = "Hello World!";
+  
+  printf("%s\n", greetings);
+  printf("%s\n", greetings2);
+  
+  printf("%lu\n", sizeof(greetings));
+  printf("%lu\n", sizeof(greetings2));
+
+  return 0;
+}
