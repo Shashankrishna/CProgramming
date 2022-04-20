82abf505f75e9d39c016857b95b0e2152c65225c
Shashankrishna <47184553+Shashankrishna@users.noreply.github.com>
2022-04-20 06:12:03 -0400

    Add files via upload

---
+++ Program57.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  printf("%f\n", sqrt(16));
+  printf("%f\n", ceil(1.4));
+  printf("%f\n", floor(1.4));
+  printf("%f", pow(4, 3));
+  return 0;
+}
+
+/* 
+
+abs(x)  :	Returns the absolute value of x
+acos(x)	:   Returns the arccosine of x
+asin(x)	:   Returns the arcsine of x
+atan(x)	:   Returns the arctangent of x
+cbrt(x)	:   Returns the cube root of x
+cos(x)	:   Returns the cosine of x
+exp(x)	:   Returns the value of Ex
+sin(x)	:   Returns the sine of x (x is in radians)
+tan(x)	:   Returns the tangent of an angle
+
+*/
