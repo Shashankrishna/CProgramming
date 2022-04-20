8d58a8ed9d85391f702c041a7490b5ad64bf2c03
Shashankrishna <47184553+Shashankrishna@users.noreply.github.com>
2022-04-20 00:36:37 -0400

    Add files via upload

---
+++ Program20.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  // When you declare a constant variable, it must be assigned with a value:
+  const int minutesPerHour;
+  minutesPerHour = 60;
+  
+  printf("%d", minutesPerHour);
+  return 0;
+}
