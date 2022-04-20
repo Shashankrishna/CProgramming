76474de867e1c799248cae4aa9af2a3a1358ad7f
Shashankrishna <47184553+Shashankrishna@users.noreply.github.com>
2022-04-20 00:28:11 -0400

    Delete Program9.c

--- Program9.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  /* The code below will print the words Hello World!
-  to the screen, and it is amazing */ 
-  // Single line comment.
-  printf("Hello World!");
-  return 0;
-}
