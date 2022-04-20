59ec6b977ed5433382df44f691e394854253e95e
Shashankrishna <47184553+Shashankrishna@users.noreply.github.com>
2022-04-20 04:02:38 -0400

    Add files via upload

---
+++ Program41.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  char greetings[] = "Hello World!";
+  //printf("%s\n", greetings);
+  //printf("%c", greetings[0]);
+
+  greetings[0] = 'J';
+  printf("%s", greetings);
+
+  return 0;
+}
