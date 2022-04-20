6a3e02325161ccfcd31f0d9513e8604d6e6c1a94
Shashankrishna <47184553+Shashankrishna@users.noreply.github.com>
2022-04-20 00:37:12 -0400

    Add files via upload

---
+++ Program22.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int myNum = 100 + 50;
+  char myChar = 'S';
+  char name[] = "Shashank!";
+
+  printf("%d\n", myNum);
+  printf("%c\n", myChar);
+  printf("%s\n", name);
+  return 0;
+}
